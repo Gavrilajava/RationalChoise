e9eb954db94e5e8b6b7fa595716122c5c235f7cb
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-08-17 09:18:52 -0500

    clean up"

--- client/src/components/AddCriterium.js
+++ client/src/components/AddCriterium.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Button from '@material-ui/core/Button';
 import {connect} from 'react-redux'
 import { API_ROOT, get_headers } from '../constants/api';
-import teal from '@material-ui/core/colors/teal';
 
 
 
